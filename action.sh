f767577e012d43afcaa555531c9fe57f30c28db3
TANYA-OP <88618459+TANYA-OP@users.noreply.github.com>
2021-08-10 08:32:57 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,5 @@
-#
-git clone https://github.com/TANYA-OP/patriciamusic2.0 /root/PatriciaMusic2.0
-cp patriciamusic2.0/.env /root/patriciamusic2.0
-cd /root/patriciamusic2.0
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t patriciamusic2.0
-docker run --privileged --env-file .env --rm -i patriciamusic2.0
+git clone https://github.com/TANYA-OP/InnexiaMusic /root/innexia
+cp innexiamusic/.env /root/innexia
+cd /root/innexia
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t innexiamusic
+docker run --privileged --env-file .env --rm -i innexiamusic
