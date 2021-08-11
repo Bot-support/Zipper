12436f8b088d09eccd65225635b712af80e6d3dc
TANYA-OP <88618459+TANYA-OP@users.noreply.github.com>
2021-08-11 07:10:21 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/TANYA-OP/daisyXmusic /root/innexia
-cp daisyXmusic/.env /root/innexia
+git clone https://github.com/TANYA-OP/daisyxmusic /root/innexia
+cp daisyxmusic/.env /root/innexia
 cd /root/innexia
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t daisyXmusic
-docker run --privileged --env-file .env --rm -i daisyXmusic
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t daisyxmusic
+docker run --privileged --env-file .env --rm -i daisyxmusic
