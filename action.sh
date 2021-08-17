81ba76ccf15ac44bf652a80e86e51906d958b109
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-08-17 13:29:28 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 #
-git clone https://github.com/TANYA-OP/innexiamusic /root/innexia
+git clone https://github.com/innexiamusic /root/innexia
 cp innexiamusic/.env /root/innexia
 cd /root/innexia
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t innexiamusic
